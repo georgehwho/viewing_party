5b305c4c4771cb0ef02eda54dfbe15e63d441f1a
George Ho <george.h.w.ho@gmail.com>
2021-05-10 21:07:44 -0400

    Fix rspec

---
+++ spec/models/user_test.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    # it { should validate_presence_of(:password) }
+  end
+end
