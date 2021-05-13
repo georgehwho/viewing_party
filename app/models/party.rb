cffc0add25a20dbc50c191d68066f105fc045695
George Ho <george.h.w.ho@gmail.com>
2021-05-12 23:47:22 -0400

    Add party validations

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,10 @@
 class Party < ApplicationRecord
+  validates :title, presence: true
+  validates :duration, presence: true
+  validates :date, presence: true
+  validates :start_time, presence: true
+
+
   belongs_to :host, class_name: :User
 
   has_many :party_guests
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Party, type: :model do
     it { should belong_to(:host) }
     it { should have_many(:party_guests) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:duration) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:start_time) }
+  end
 end
