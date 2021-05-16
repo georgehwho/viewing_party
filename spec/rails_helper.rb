a73118f435b6f9a351744f02590a4690ea4e4bad
George Ho <george.h.w.ho@gmail.com>
2021-05-16 14:35:16 -0400

    Add new vcr fixtures after setting match request on to ignore url params

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,5 +77,10 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data('<DONT_SHARE_MY_THEMOVIESDB_KEY>') { ENV['themoviesdb_key'] }
   config.configure_rspec_metadata!
-  config.default_cassette_options = { re_record_interval: 7.days }
+  config.default_cassette_options = {
+    re_record_interval: 7.days,
+    :match_requests_on => [:method,
+      VCR.request_matchers.uri_without_param(:url)]
+  }
+
 end
