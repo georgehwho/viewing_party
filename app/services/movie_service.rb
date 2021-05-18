f7832475d2d61ad7db129bddeb7843f73d3764e4
George Ho <george.h.w.ho@gmail.com>
2021-05-18 12:19:41 -0400

    Add api key to get vs new

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,16 +1,16 @@
 class MovieService
   class << self
     def request_api(path)
-      resp = conn.get(path)
+      resp = conn.get(path) do |faraday|
+        faraday.params['api_key'] = ENV['themoviesdb_key']
+      end
       parse_json(resp)
     end
 
     private
 
     def conn
-      Faraday.new(url: 'https://api.themoviedb.org/3') do |faraday|
-        faraday.params['api_key'] = ENV['themoviesdb_key']
-      end
+      Faraday.new(url: 'https://api.themoviedb.org/3')
     end
 
     def parse_json(response)
